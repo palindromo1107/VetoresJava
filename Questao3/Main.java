9c166a83bef34ec47a9b6ddcdf5c6b7d13397723
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 11:19:45 -0300

    Implement array initialization and squaring logic
    Initialize two integer arrays and square elements from A into B.

---
+++ Questao3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[15];
+        int [] B = new int[15];
+
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i] * A[i];
+        }
+
+    }
+}
