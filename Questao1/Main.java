c37e2e67e8a5663f4fa28c2951a573beceb7923f
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 10:55:33 -0300

    Implement basic array copy in Main.java

---
+++ Questao1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[5];
+        int[] B = new int[5];
+        
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i];
+        }
+
+    }
+}
