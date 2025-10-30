f79594d4912271f3cc0b7877e2e34cdb316bdb56
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 11:13:15 -0300

    Implement array doubling in Main.java

---
+++ Questao2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[8];
+        int [] B = new int[8];
+
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i] * 2;
+        }
+
+    }
+}
