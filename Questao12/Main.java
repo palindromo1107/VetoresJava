643a624b20b2b361291a724c5aea62bad62ddadc
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 14:04:35 -0300

    Implement main method to sum array elements

---
+++ Questao12/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int[] A = {1,2,3,4,5,6,7,8,9,0};
+	    
+	    int total = 0;
+	    
+	    for (int i = 0; i < A.length; i++) {
+	        
+	        total += A[i];
+	        
+	    }
+	    
+	    System.out.print("total: " + total);
+		
+	}
+}
