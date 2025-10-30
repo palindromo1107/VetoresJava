7f3b6e9256b225d194ce9c2af84178633b577937
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 11:34:58 -0300

    Implement main class with array and square root calculation

---
+++ Questao4/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[15];
+        double[] B = new double[15];
+
+        for (int i = 0; i < A.length; i++) {
+            B[i] = Math.sqrt(A[i]);
+        }
+
+    }
+}
