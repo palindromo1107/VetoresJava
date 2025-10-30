61451e92b57de0ac635acad22d89d6c95bfaddf8
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 11:41:57 -0300

    Implement array initialization and summation
    Initialize arrays A, B, and C, and compute the sum of A and B into C.

---
+++ Questao6/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[10];
+        int[] B = new int[10];
+        int[] C = new int[10];
+
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i] + B[i];
+        }
+
+    }
+}
